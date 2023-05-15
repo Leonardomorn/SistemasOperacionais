f3293b3905bb14c57a4c3c083c39b8566485312a
Camaleoao <camargo.leonardo1@gmail.com>
2023-05-15 13:22:44 -0300

    adicionado diretório P5

---
+++ P5/ppos_data.h
@@ -0,0 +1,59 @@
+// PingPongOS - PingPong Operating System
+// Prof. Carlos A. Maziero, DINF UFPR
+// Versão 1.5 -- Março de 2023
+
+// Estruturas de dados internas do sistema operacional
+
+#ifndef __PPOS_DATA__
+#define __PPOS_DATA__
+
+#define READY      0
+#define TERMINATED 1
+#define SUSPENDED  2
+
+#include <ucontext.h>		// biblioteca POSIX de trocas de contexto
+
+// Estrutura que define um Task Control Block (TCB)
+typedef struct task_t
+{
+  struct task_t *prev, *next ;		// ponteiros para usar em filas
+  int id ;				// identificador da tarefa
+  ucontext_t context ;			// contexto armazenado da tarefa
+  short status ;			// pronta, rodando, suspensa, ...
+  short priority_static;
+  short priority_dynamic;     //prioridade da tarefa a ser executada, -20 é prioridade máxima e 20 prioridade mínima
+  // ... (outros campos serão adicionados mais tarde)
+} task_t ;
+
+// estrutura que define um semáforo
+typedef struct
+{
+  // preencher quando necessário
+} semaphore_t ;
+
+// estrutura que define um mutex
+typedef struct
+{
+  // preencher quando necessário
+} mutex_t ;
+
+// estrutura que define uma barreira
+typedef struct
+{
+  // preencher quando necessário
+} barrier_t ;
+
+// estrutura que define uma fila de mensagens
+typedef struct
+{
+  // preencher quando necessário
+} mqueue_t ;
+
+#endif
+
+
+void dispatcher ();
+task_t * scheduler ();
+task_t* first_min();
+void raise_priority(task_t* to_be_executed_task);
+void restore_dynamic__into_static_prio(task_t *taskAux);
